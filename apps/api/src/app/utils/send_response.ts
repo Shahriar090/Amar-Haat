8e1e552b50377678ce53a3edf8567447bb36d7d2
Shahriar Hossain <shahriarh655@gmail.com>
2025-11-17 23:20:13 +0600

    chore(config): Implemented two utility functions async_Handler and send_response to prevent redundant using of try-catch block and sending json response

---
+++ apps/api/src/app/utils/send_response.ts
@@ -0,0 +1,19 @@
+import type { Response } from 'express';
+
+type ResponseType<T> = {
+	statusCode: number;
+	success: boolean;
+	message?: string;
+	data: T;
+};
+
+const sendResponse = <T>(res: Response, data: ResponseType<T>) => {
+	res.status(data?.statusCode).json({
+		statusCode: data?.statusCode,
+		success: data?.success,
+		message: data?.message,
+		data: data?.data,
+	});
+};
+
+export default sendResponse;
